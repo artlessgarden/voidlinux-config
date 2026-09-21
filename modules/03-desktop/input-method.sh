8fadec1ec33a27deaba805ed4001e47032b740f5
less <less@less>
2026-09-21 14:56:35 +0700

    limit fcitx config links to managed files

--- modules/03-desktop/input-method.sh
+++ modules/03-desktop/input-method.sh
@@ -8,6 +8,6 @@ sudo xbps-install -Sy -y fcitx5 fcitx5-chinese-addons
 mkdir -p "$HOME/.config/fcitx5/conf"
 ln -sfn "$h/.config/fcitx5/profile" "$HOME/.config/fcitx5/profile"
 ln -sfn "$h/.config/fcitx5/config" "$HOME/.config/fcitx5/config"
-for source in "$h/.config/fcitx5/conf/"*.conf; do
-	ln -sfn "$source" "$HOME/.config/fcitx5/conf/$(basename "$source")"
+for name in classicui.conf pinyin.conf clipboard.conf; do
+	ln -sfn "$h/.config/fcitx5/conf/$name" "$HOME/.config/fcitx5/conf/$name"
 done
